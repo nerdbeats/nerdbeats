d746f6bc6f7fd42681f97544901df3c54105300b
ziflex <ziflex@gmail.com>
2015-01-24 15:38:43 +0100

    Updated 'AudioLoader'

--- client/app/services/audio-loader.js
+++ client/app/services/audio-loader.js
@@ -1,7 +1,11 @@
-window.app.service('AudioLoaderService', function ($http, $q, SoundCloudService, AudioContext) {
+window.app.service('AudioLoaderService', function ($http, $q, SoundCloudService, AudioContext, BufferAudioSourceUnit, StreamAudioSourceUnit) {
   return {
     getStream: function (id) {
-      return SoundCloudService.stream(id);
+      return SoundCloudService.stream(id).then(function (sound) {
+        var unit = new StreamAudioSourceUnit();
+        unit.src(sound.src);
+        return unit;
+      });
     },
     getBuffer: function (id) {
       var def = $q.defer();
@@ -11,7 +15,9 @@ window.app.service('AudioLoaderService', function ($http, $q, SoundCloudService,
       }).then(function (response, err) {
         if (!err) {
           AudioContext.decodeAudioData(response.data, function (buffer) {
-            def.resolve(buffer);
+            var unit = new BufferAudioSourceUnit();
+            unit.src(buffer);
+            def.resolve(unit);
           });
         } else {
           def.reject(err);
