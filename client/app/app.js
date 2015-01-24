11603adc662dea96bb53b7bbb8072d5078865ec9
mmajewski <mmajewski@infusion.com>
2015-01-25 00:24:39 +0100

    responsive

--- client/app/app.js
+++ client/app/app.js
@@ -6,7 +6,8 @@ window.app = angular.module('nerdBeats', [
   'ngSanitize',
   'ngRoute',
   'ui.bootstrap',
-  'ui.bootstrap-slider'
+  'ui.bootstrap-slider',
+  'angular-velocity'
 ]);
 
 window.app.config(function ($routeProvider, $locationProvider) {
@@ -17,3 +18,44 @@ window.app.config(function ($routeProvider, $locationProvider) {
 
     $locationProvider.html5Mode(true);
   });
+
+
+
+
+
+
+var autofit = function() {
+  // initial fixed width
+  var minW = 1920;
+
+  if ($(window).width() < minW) {
+    var ratio = $(window).width() / minW;
+
+    // For chrome and safari, use zoom
+    var detect = navigator.userAgent.toLowerCase();
+    if((detect.indexOf('chrome') + 1) || (detect.indexOf('safari') + 1)) {
+      $(document.body).css('zoom', ratio);
+    } else {
+      // Other browser that doesn't support zoom, use -moz-transform to scale and compensate for lost width
+      $('#fit-wrap').css('-webkit-transform', "scale(" + ratio + ")");
+      $('#fit-wrap').css('-moz-transform', "scale(" + ratio + ")");
+      $('#fit-wrap').css('-ms-transform', "scale(" + ratio + ")");
+      $('#fit-wrap').css('transform', "scale(" + ratio + ")");
+      $('#fit-wrap').width($(window).width() / ratio + 10);
+    }
+  } else {
+    $(document.body).css('zoom', '');
+    $('#fit-wrap').css('-webkit-transform', "");
+    $('#fit-wrap').css('-moz-transform', "");
+    $('#fit-wrap').css('-ms-transform', "");
+    $('#fit-wrap').css('transform', "");
+    $('#fit-wrap').width("");
+  }
+}
+
+
+autofit();
+
+$(window).resize(function() {
+  autofit();
+});
