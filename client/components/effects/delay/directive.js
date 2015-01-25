5435c86f912806db74e18470c89163d1e06fb423
ziflex <ziflex@gmail.com>
2015-01-25 19:55:14 +0100

    Reconfigured 'Delay' effect

--- client/components/effects/delay/directive.js
+++ client/components/effects/delay/directive.js
@@ -4,10 +4,11 @@ window.app.directive('delayEffect', function () {
     templateUrl: 'components/effects/delay/template.html',
     link: function ($scope) {
       var node = $scope.node;
+      node.delayTime(850);
+      node.feedback(0.65);
+      node.cutoff(1500);
       $scope.$watch('value', function (newValue) {
-        node.wetLevel(newValue * 100);
-        node.delayTime(newValue * 100);
-        node.feedback(newValue);
+        node.wetLevel(newValue * 2);
         node.bypass(newValue === 0);
       });
     },
